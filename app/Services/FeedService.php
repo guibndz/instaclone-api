6ab69367d5067bcc7da84842d5f97d9faeac2171
guibondezan <gui.gastaldo@gmail.com>
2026-04-24 15:17:37 -0300

    feat: add likes and comments count to feed

--- app/Services/FeedService.php
+++ app/Services/FeedService.php
@@ -7,14 +7,14 @@ use Illuminate\Support\Facades\Auth;
 
 class FeedService
 {
-    public function getFeed()
+public function getFeed()
     {
         $user = Auth::user();
-
         $followingIds = $user->following()->pluck('users.id');
 
         return Post::whereIn('user_id', $followingIds)
             ->with('user:id,name,username,avatar_url')
+            ->withCount(['likes', 'comments']) 
             ->latest()
             ->cursorPaginate(10);
     }
